7e9d7a7934dc21122da6d6a6e809fb7571d3da5b
Orçun Aşık <asikorcun@gmail.com>
2023-01-08 23:46:49 +0300

    Created Models

---
+++ EReconciliationAPI/EReconciliationAPI.Entities/Concrete/BaBsReconciliation.cs
@@ -0,0 +1,28 @@
+﻿using EReconciliationAPI.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EReconciliationAPI.Entities.Concrete
+{
+    public class BaBsReconciliation : IEntity
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public int CurrencyAccountId { get; set; }
+        public string Type { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public int Quantity { get; set; }
+        public decimal Total { get; set; }
+        public bool IsSendEMail { get; set; }
+        public DateTime? SendEMailDate { get; set; }
+        public bool? IsEmailRead { get; set; }
+        public DateTime? EmailReadDate { get; set; }
+        public bool? IsResultSucceed { get; set; }
+        public DateTime? ResultDate { get; set; }
+        public string? ResultNote { get; set; }
+    }
+}
