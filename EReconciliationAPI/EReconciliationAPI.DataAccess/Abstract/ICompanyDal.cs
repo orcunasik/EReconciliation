a9e8f81ee59042a8e0d92a5c11a9e38ef439b261
Orçun Aşık <asikorcun@gmail.com>
2023-01-09 00:01:20 +0300

    Created was interfaces that implement GenericRepo interfaces

---
+++ EReconciliationAPI/EReconciliationAPI.DataAccess/Abstract/ICompanyDal.cs
@@ -0,0 +1,16 @@
+﻿using EReconciliationAPI.Core.DataAccess;
+using EReconciliationAPI.Core.DataAccess.EntityFramework;
+using EReconciliationAPI.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EReconciliationAPI.DataAccess.Abstract
+{
+    public interface ICompanyDal : IEntityRepository<Company>
+    {
+        
+    }
+}
---
+++ EReconciliationAPI/EReconciliationAPI.DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,14 @@
+﻿using EReconciliationAPI.Core.DataAccess;
+using EReconciliationAPI.Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EReconciliationAPI.DataAccess.Abstract
+{
+    public interface IUserDal : IEntityRepository<User>
+    {
+    }
+}
