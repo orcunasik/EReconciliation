216c6b1dd29065a501fa2ef7609c202e61db6474
Orçun Aşık <asikorcun@gmail.com>
2023-01-09 00:04:08 +0300

    Created was classes that implement self interfaces with GenericRepo class

---
+++ EReconciliationAPI/EReconciliationAPI.DataAccess/Concrete/EntityFramework/EfAccountReconciliationDetailDal.cs
@@ -0,0 +1,16 @@
+﻿using EReconciliationAPI.Core.DataAccess.EntityFramework;
+using EReconciliationAPI.DataAccess.Abstract;
+using EReconciliationAPI.DataAccess.Concrete.EntityFramework.Context;
+using EReconciliationAPI.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EReconciliationAPI.DataAccess.Concrete.EntityFramework
+{
+    public class EfAccountReconciliationDetailDal : EfEntityRepository<AccountReconciliationDetail, EReconciliationContext>, IAccountReconciliationDetailDal
+    {
+    }
+}
---
+++ EReconciliationAPI/EReconciliationAPI.DataAccess/Concrete/EntityFramework/EfUserOperationClaimDal.cs
@@ -0,0 +1,17 @@
+﻿using EReconciliationAPI.Core.DataAccess.EntityFramework;
+using EReconciliationAPI.Core.Entities.Concrete;
+using EReconciliationAPI.DataAccess.Abstract;
+using EReconciliationAPI.DataAccess.Concrete.EntityFramework.Context;
+using EReconciliationAPI.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EReconciliationAPI.DataAccess.Concrete.EntityFramework
+{
+    public class EfUserOperationClaimDal : EfEntityRepository<UserOperationClaim, EReconciliationContext>, IUserOperationClaimDal
+    {
+    }
+}
