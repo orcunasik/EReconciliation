4eaa0198d609c170aa072f8d3559c3728bb6be79
Orçun Aşık <asikorcun@gmail.com>
2023-01-08 23:58:38 +0300

    Created Context class for DB

---
+++ EReconciliationAPI/EReconciliationAPI.DataAccess/Concrete/EntityFramework/Context/EReconciliationContext.cs
@@ -0,0 +1,31 @@
+﻿using EReconciliationAPI.Core.Entities.Concrete;
+using EReconciliationAPI.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EReconciliationAPI.DataAccess.Concrete.EntityFramework.Context
+{
+    public class EReconciliationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-I6RA8LI;Database=EReconciliation;Integrated Security=true;");
+        }
+        public DbSet<AccountReconciliation> AccountReconciliations { get; set; }
+        public DbSet<AccountReconciliationDetail> AccountReconciliationDetails { get; set; }
+        public DbSet<BaBsReconciliation> BaBsReconciliations { get; set; }
+        public DbSet<BaBsReconciliationDetail> BaBsReconciliationDetails { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<CurrencyAccount> CurrencyAccounts { get; set; }
+        public DbSet<MailParameter> MailParameters { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserCompany> UserCompanies { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    }
+}
