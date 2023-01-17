7629e463e885e845912ef201708d21665a2e94a8
Orçun Aşık <asikorcun@gmail.com>
2023-01-17 19:21:59 +0300

    GetList and Save method signatures have been added to the created Company interface.

---
+++ EReconciliationAPI/EReconciliationAPI.Business/Abstract/ICompanyService.cs
@@ -0,0 +1,16 @@
+﻿using EReconciliationAPI.Core.Utilities.Results.Abstract;
+using EReconciliationAPI.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EReconciliationAPI.Business.Abstract
+{
+    public interface ICompanyService
+    {
+        IResult Save (Company company);
+        IDataResult<List<Company>> GetList();
+    }
+}
