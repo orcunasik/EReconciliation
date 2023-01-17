72d284b8aab406a37948a1618c52fc4e0c4eebf7
Orçun Aşık <asikorcun@gmail.com>
2023-01-17 19:26:53 +0300

    A constant class named Messages was created instead of "Magic String" for Success or Error status.

---
+++ EReconciliationAPI/EReconciliationAPI.Business/Constants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EReconciliationAPI.Business.Constants
+{
+    public class Messages
+    {
+        public static string CompanySuccessfullySaved = "Şirket Kaydı Başarıyla Gerçekleşti";
+        public static string GetListCompany = "Tüm Şirketler Başarıyla Listelendi";
+    }
+}
