d95551ccb02dc6266e180cb69d45a36c29dea5dc
Orçun Aşık <asikorcun@gmail.com>
2023-01-17 19:24:31 +0300

    The created Company Service interface was included in the concrete class and the methods are populated.

---
+++ EReconciliationAPI/EReconciliationAPI.Business/Concrete/CompanyService.cs
@@ -0,0 +1,38 @@
+﻿using EReconciliationAPI.Business.Abstract;
+using EReconciliationAPI.Business.Constants;
+using EReconciliationAPI.Core.Utilities.Results.Abstract;
+using EReconciliationAPI.Core.Utilities.Results.Concrete;
+using EReconciliationAPI.DataAccess.Abstract;
+using EReconciliationAPI.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EReconciliationAPI.Business.Concrete
+{
+    public class CompanyService : ICompanyService
+    {
+        private readonly ICompanyDal _companyDal;
+
+        public CompanyService(ICompanyDal companyDal)
+        {
+            _companyDal = companyDal;
+        }
+        public IDataResult<List<Company>> GetList()
+        {
+            return new SuccessDataResult<List<Company>>(_companyDal.GetAll(),Messages.GetListCompany);
+        }
+
+        public IResult Save(Company company)
+        {
+            if (company.Name.Length > 10)
+            {
+                _companyDal.Add(company);
+                return new SuccessResult(Messages.CompanySuccessfullySaved);
+            }
+            return new ErrorResult("Şirket Adı En Az 10 Karakter Olmalıdır!");
+        }
+    }
+}
