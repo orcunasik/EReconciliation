db38185f8573560c60e29e9f7cb71d7404268887
Orçun Aşık <asikorcun@gmail.com>
2023-04-29 21:08:03 +0300

    Created TransactionScopeAspect

---
+++ EReconciliationAPI/EReconciliationAPI.Core/Aspects/Autofac/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,26 @@
+﻿using Castle.DynamicProxy;
+using EReconciliationAPI.Core.Utilities.Interceptors;
+using System.Transactions;
+
+namespace EReconciliationAPI.Core.Aspects.Autofac.Transaction
+{
+    public class TransactionScopeAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope transactionScope = new())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Complete();
+                }
+                catch (Exception)
+                {
+                    transactionScope.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
