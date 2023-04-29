2791de3a9cdeaeb9f9a6124de1d811bf24289317
Orçun Aşık <asikorcun@gmail.com>
2023-04-29 21:00:06 +0300

    Created validation rules

---
+++ EReconciliationAPI/EReconciliationAPI.Business/ValidationRules/FluentValidation/CompanyValidator.cs
@@ -0,0 +1,16 @@
+﻿using EReconciliationAPI.Entities.Concrete;
+using FluentValidation;
+
+namespace EReconciliationAPI.Business.ValidationRules.FluentValidation
+{
+    public class CompanyValidator : AbstractValidator<Company>
+    {
+        public CompanyValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty().WithName("Şirket Adı").WithMessage("{PropertyName} Boş Bırakılmamalıdır!")
+                                .MinimumLength(5).WithName("Şirket Adı").WithMessage("{PropertyName} En Az 5 Karakter Olmalıdır!");
+
+            RuleFor(c => c.Address).NotEmpty().WithName("Şirket Adresi").WithMessage("{PropertyName}  Boş Bırakılmamalıdır!");
+        }
+    }
+}
---
+++ EReconciliationAPI/EReconciliationAPI.Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,19 @@
+﻿using EReconciliationAPI.Core.Entities.Concrete;
+using FluentValidation;
+
+namespace EReconciliationAPI.Business.ValidationRules.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.Name)
+                .NotEmpty().WithName("Kullancı Adı").WithMessage("{PropertyName} Boş Bırakılmamalıdır!")
+                .MinimumLength(4).WithName("Kullancı Adı").WithMessage("{PropertyName} En Az 4 Karakter Olmalıdır!");
+
+            RuleFor(u => u.Email).NotEmpty().WithName("Email Adresi").WithMessage("{PropertyName} Boş Bırakılmamalıdır!")
+                                .EmailAddress().WithName("Email Adresi").WithMessage(" Geçerli Bir{PropertyName} Yazılmalıdır! Ex:example@gmail.com");
+
+        }
+    }
+}
